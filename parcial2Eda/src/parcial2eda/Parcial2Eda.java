65b96ee48afd1b17a61df7169a62436a30333e4d
dannitacode <dannavalenchu076@gmail.com>
2026-06-03 22:43:40 -0300

    Opcion 'inscribir jugadores' en menu para armar rueda de torneo post

--- parcial2Eda/src/parcial2eda/Parcial2Eda.java
+++ parcial2Eda/src/parcial2eda/Parcial2Eda.java
@@ -1,5 +1,7 @@
 package parcial2eda;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,8 +13,14 @@ public class Parcial2Eda {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Arbol torneo = new Arbol();
-        int cant_jugadores;
+        List<Participante> lista_jugadores = new ArrayList<>();
+        
+        int cant_jugadores = 0;
+        String nombre_jugador;
+        int ranking_jugador;
+        String nacionalidad_jugador = null;
         int opcion;
+        
         do {
             System.out.println("Menu");
             System.out.println("1. Ingresar cantidad de jugadores");
@@ -30,6 +38,26 @@ public class Parcial2Eda {
                     torneo.crear_torneo(cant_jugadores);
                     System.out.println("Torneo creado");
                     break;
+                case 2: 
+                    sc.nextLine();
+                    int i=1;
+                    if (cant_jugadores == 0) {
+                        System.out.println("Tiene que indicar primero la cantidad de jugadores participantes del torneo");
+                        break;
+                    }
+                    while(i<=cant_jugadores){
+                        System.out.println("/// Jugador nro " + i + " ///");
+                        System.out.println("Ingrese el nombre completo del jugador: ");
+                        nombre_jugador = sc.nextLine();
+                        System.out.println("Su nacionalidad: ");
+                        nombre_jugador = sc.nextLine();
+                        System.out.println("Ahora su puesto en el ranking: ");
+                        ranking_jugador = sc.nextInt();
+                        sc.nextLine();
+                        lista_jugadores.add(new Participante(nombre_jugador, nacionalidad_jugador, ranking_jugador));
+                        i++;
+                    }
+                    System.out.println("Los jugadores han sido inscriptos");   
             }
         } while (opcion != 3);
     }
