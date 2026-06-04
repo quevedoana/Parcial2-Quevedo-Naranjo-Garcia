b1997ae41016a0856c0921b3aecc6873dba5337e
dannitacode <dannavalenchu076@gmail.com>
2026-06-03 22:23:11 -0300

    Getters y setters a la clase 'Participante'

--- parcial2Eda/src/parcial2eda/Participante.java
+++ parcial2Eda/src/parcial2eda/Participante.java
@@ -16,6 +16,30 @@ public class Participante {
         ranking = rank;
     }
 
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+    
     @Override
     public String toString() {
         return "Participante: " + "\nNombre: " + nombre_completo + "\nNacionalidad: " + nacionalidad + "\nRanking: " + ranking;
